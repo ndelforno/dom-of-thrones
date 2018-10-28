bca92a60256cf28fe3a625f5b90cd08bbb3ee048
Ndelforno <nicolas.delforno@gmail.com>
2018-10-28 10:58:16 -0400

    setup stage scene2

---
+++ js/scene2.js
@@ -0,0 +1,36 @@
+function scene2() {
+  console.log('Executing scene 2!');
+
+  // Setup Stage
+  stage = document.querySelector('#stage')
+  castle = document.querySelector('#castle')
+  stageImg = stage.querySelector('#stage-img')
+  castleImgSrc = castle.getAttribute('src')
+  stageImg.setAttribute('src', castleImgSrc)
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
